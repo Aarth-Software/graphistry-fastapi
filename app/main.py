aad278a7969f80dd00279318a0e06f0611716a45
Jagadeesh Garapati <jagadeesh@aarth.io>
2023-03-03 11:50:03 +0530

    made changes to query2 where there was spaces in between output params

--- app/main.py
+++ app/main.py
@@ -272,28 +272,28 @@ RETURN labels.JournalPublication as Journals, labels.JournalReference as Papers,
 
 query2 = """MATCH (ref:JournalReference)-[:APPEARED_IN]->(pub:JournalPublication)
 WHERE '1981'> ref.year<= '1990'
-    with pub.name as `Journal Name`, "1980-1990" as Years, count(ref) as `No.Of Papers`
-RETURN `Journal Name`, Years, `No.Of Papers`
+    with pub.name as JournalName, "1980-1990" as Years, count(ref) as NumOfPapers
+RETURN JournalName, Years, NumOfPapers
 UNION ALL
 MATCH (ref:JournalReference)-[:APPEARED_IN]->(pub:JournalPublication)
 WHERE '1991'> ref.year<= '2000'
-    with pub.name as `Journal Name`, "1991-2000" as Years, count(ref) as `No.Of Papers`
-RETURN `Journal Name`, Years, `No.Of Papers`
+    with pub.name as JournalName, "1991-2000" as Years, count(ref) as NumOfPapers
+RETURN JournalName, Years, NumOfPapers
 UNION ALL
 MATCH (ref:JournalReference)-[:APPEARED_IN]->(pub:JournalPublication)
 WHERE '2001'> ref.year<= '2010'
-    with pub.name as `Journal Name`, "2001-2010" as Years, count(ref) as `No.Of Papers`
-RETURN `Journal Name`, Years, `No.Of Papers`
+    with pub.name as JournalName, "2001-2010" as Years, count(ref) as NumOfPapers
+RETURN JournalName, Years, NumOfPapers
 UNION ALL
 MATCH (ref:JournalReference)-[:APPEARED_IN]->(pub:JournalPublication)
 WHERE '2011'> ref.year<= '2020'
-    with pub.name as `Journal Name`, "2011-2020" as Years, count(ref) as `No.Of Papers`
-RETURN `Journal Name`, Years, `No.Of Papers`
+    with pub.name as JournalName, "2011-2020" as Years, count(ref) as NumOfPapers
+RETURN JournalName, Years, NumOfPapers
 UNION ALL
 MATCH (ref:JournalReference)-[:APPEARED_IN]->(pub:JournalPublication)
 WHERE '2020'> ref.year<= '2023'
-    with pub.name as `Journal Name`, "2020-2023" as Years, count(ref) as `No.Of Papers`
-RETURN `Journal Name`, Years, `No.Of Papers`
+    with pub.name as JournalName, "2020-2023" as Years, count(ref) as NumOfPapers
+RETURN JournalName, Years, NumOfPapers
 """
 
 query3 = """MATCH (c:Construct)-[r:AS]->(cr:ConstructRole)
